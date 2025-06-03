ddc9fae3dce5232abb06e34cc7ac323f4d2ea109
groot <abel.mulugeta.dagne@gmail.com>
2025-06-03 22:44:36 +0300

    update formatting

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,6 +18,7 @@ def pascal_triangle(n):
         for i in range(n):
             row = [1]
             for j in range(1, i + 1):
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j] if i != j else 1)
+                row.append(triangle[i - 1][j - 1] \
+                   + triangle[i - 1][j] if i != j else 1)
             triangle.append(row)
     return triangle
