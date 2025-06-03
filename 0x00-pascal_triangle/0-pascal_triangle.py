18761ad7724331cfe4597dbf88f846a0f46e478b
groot <abel.mulugeta.dagne@gmail.com>
2025-06-03 22:36:27 +0300

    0x00-pascal_triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """generates a pascal triangle
+
+    Args:
+        n (int): number of rows
+
+    Returns:
+        list(list(int)): list of lists of integers
+    """
+    triangle = list()
+    if n > 0:
+        for i in range(n):
+            row = [1]
+            for j in range(1, i + 1):
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j] if i != j else 1)
+            triangle.append(row)
+    return triangle
