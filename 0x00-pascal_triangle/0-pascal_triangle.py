83405c4106fd7ac3932729aa11387c3d008ea9db
groot <abel.mulugeta.dagne@gmail.com>
2025-06-06 13:35:37 +0300

    minimum-opertion

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,22 +5,20 @@
 
 
 def pascal_triangle(n):
-    """Generates a Pascal's triangle.
+    """generates a pascal triangle
 
     Args:
-        n (int): Number of rows.
+        n (int): number of rows
 
     Returns:
-        list(list(int)): List of lists of integers representing the triangle.
+        list(list(int)): list of lists of integers
     """
     triangle = list()
     if n > 0:
         for i in range(n):
             row = [1]
             for j in range(1, i + 1):
-                if i != j:
-                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-                else:
-                    row.append(1)
+                row.append(triangle[i - 1][j - 1] +
+                           triangle[i - 1][j] if i != j else 1)
             triangle.append(row)
     return triangle
